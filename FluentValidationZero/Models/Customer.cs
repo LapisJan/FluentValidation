22f2f4d7ee17596ddc97d29e526f3e98bc02338d
ploypailin <ploypailin.wongpichit@kingpower.com>
2021-01-24 13:03:35 +0700

    first commit

---
+++ FluentValidationZero/Models/Customer.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+
+namespace FluentValidationZero.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Firstname).NotEmpty();
+            RuleFor(x => x.Lastname).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+        }
+    }
+}
---
+++ FluentValidationZero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentValidationZero.Models;
+
+namespace FluentValidationZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer()
+            {
+                Id = 1,
+                Firstname = "",
+                Lastname = "wongpichit",
+                Address = "kingpower"
+            };
+            
+            var results = new CustomerValidator().Validate(customer);
+            
+            if(!results.IsValid) {
+                foreach(var failure in results.Errors) {
+                    Console.WriteLine(failure.PropertyName + " Error : " + failure.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data is valid.");
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
